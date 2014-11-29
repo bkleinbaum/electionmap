9bbd8e78257887ad5e4a70b57f60d9825b1ed4c3
bkleinbaum <benkleinbaum@Bens-MacBook-Pro.local>
2014-11-29 15:15:21 -0500

    Merge branch 'gh-pages'

--- js/script.js
+++ js/script.js
@@ -2,17 +2,15 @@
 
 var map = L.map('map');
 map.setView([40.681339, -73.96302], 12
+
 	);
 
 
-// var base = L.tileLayer('http://{s}.tiles.mapbox.com/v3/examples.map-i875mjb7/{z}/{x}/{y}.png', {
-//     maxZoom: 14
-// }).addTo(map)
 
 var Esri_WorldGrayCanvas = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
-	attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
-	maxZoom: 14,
-	minZoom: 9
+	attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ'
+	// maxZoom: 14,
+	// minZoom: 9
 }).addTo(map);
 
 L.mapbox.accessToken = 'pk.eyJ1IjoiYms3NDEiLCJhIjoiZFNVcTNvdyJ9.h8G4i4ib7PicRCiejvZW6g';
@@ -21,12 +19,97 @@ var mapboxTiles = L.tileLayer('https://{s}.tiles.mapbox.com/v3/bk741.b5k4kj4i/{z
 
 
 
+// import nabe layer
+
+// function makeNabeMarkers (feature,layer) {	
+// 	layer.bindPopup(
+// 		"<b>"
+// 		+feature.properties.neighborho
+// 		+"</b>"
+// 		)};
+
+
+// var nabeMarkerOptions = {
+//     radius: 8,
+//     fillColor: "#ff7800",
+//     color: "#000",
+//     weight: 1,
+//     opacity: 1,
+//     fillOpacity: 0.8
+// };
+
+// L.geoJson(nabecentroid, {
+// 	onEachFeature: makeNabeMarkers, 
+//     pointToLayer: function (feature, latlng) {
+//         return L.circleMarker(latlng, nabeMarkerOptions);
+//     }
+// }).addTo(map);
+
+
+//import mayor layer
+
+var mayorStyle = {
+    "weight": .5,
+    "fillOpacity": 0,
+    "color": 'black'
+   };
+
+function makeMayorMarkers (feature,layer) {
+	layer.bindPopup(
+		"<b> Election District: </b>"+feature.properties.ADED	
+		+"<br>"
+		+"<b>de Blasio: </b>"+feature.properties.deBlasio
+		+"<br>"
+		+"<b>Liu: </b>"+feature.properties.Liu
+		+"<br>"
+		+"<b>Quinn: </b>"+feature.properties.Quinn
+		+"<br>"
+		+"<b>Thompson: </b>"+feature.properties.Thompson
+		+"<br>"
+		+"<b>Weiner: </b>"+feature.properties.Weiner
+		+"<br>"
+		+"<b>Total Votes: </b>"+feature.properties.Total
+	 
+		)};
 
 
 
-// import layers
+var mayorED = new L.GeoJSON.AJAX("webdata/mayored.geojson", {
+	style: mayorStyle,
+	onEachFeature: makeMayorMarkers
+}).addTo(map);
 
-var PEMayorED = omnivore.topojson("webdata/PEMayorED.topojson").addTo(map);
+
+//import marker layer
+
+//Add labels layer
+var labelStyle = {
+    color: '#CCC',
+    opacity: 1
+};
+
+var labelMarkerOptions = {
+        opacity: 0,
+        fillOpacity: 0
+};
+
+var labelLayer = L.geoJson(nabecentroid, {
+        pointToLayer: function (feature, latlng) {
+                return L.circleMarker(latlng, labelMarkerOptions);
+        },
+        onEachFeature: function (feature, layer) {
+                layer.bindLabel(feature.properties.neighborho, {noHide:true});
+        }
+});
+
+labelLayer.eachLayer(function(l) {l.showLabel();});
+map.addLayer(labelLayer);
+layerControl.addOverlay(labelLayer, 'Site Labels');
+
+
+
+
+
 
 
 
